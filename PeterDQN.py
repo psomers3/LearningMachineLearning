2643e17d90825624c3a38048f6b1e34612acae9a
Peter Somers <psomers3@gatech.edu>
2019-06-15 11:55:53 +0200

    simplified script

--- PeterDQN.py
+++ PeterDQN.py
@@ -9,62 +9,58 @@ env._max_episode_steps = sim_step_limit
 observation_dim = env.observation_space.shape[0]
 action_dim = env.action_space.n
 
-hidden_layer_specs = list()
-hidden_layer_specs.append({'layer': 'dense',
+layer_specs = list()
+layer_specs.append({'layer': 'dense',
                            'units': 8,
                            'activation': 'relu'})
-hidden_layer_specs.append({'layer': 'dense',
+layer_specs.append({'layer': 'dense',
                            'units': 8,
                            'activation': 'relu'})
-hidden_layer_specs.append({'layer': 'dense',
+layer_specs.append({'layer': 'dense',
                            'units': action_dim,
                            'activation': 'softmax'})
 
-myAgent = DQN(input_shape=(None, observation_dim), layers=hidden_layer_specs)
+myAgent = DQN(input_shape=(None, observation_dim), layers=layer_specs, experience_length=100)
 myAgent.initiate()
+myAgent.set_to_learn(True)
 
-max_ep = 1000  # Set total number of episodes to train agent on.
+epoc_length = 100
 
-i = 0
+k = 0
 total_num_steps = 0
 try:
-    while i < max_ep:
-        s = env.reset()
-        for j in range(sim_step_limit):
-            # Probabilistically pick an action given our network outputs.
-            a_disc = myAgent.make_decision(s)
-            a = np.random.choice(a_disc[0], p=a_disc[0])
-            a = np.argmax(a_disc == a)
+    while True:
+        for i in range(epoc_length):
+            s = env.reset()
+            for j in range(sim_step_limit):
+                # Probabilistically pick an action given our network outputs.
+                a = myAgent.make_decision(s)
 
-            s1, r, done, _ = env.step(a)  # Get our reward for taking an action given a bandit.
-            # s1 = [position of cart, velocity of cart, angle of pole, rotation rate of pole]
-            r = 1/np.linalg.norm([s1[0]*50, s1[1]*10, s1[2]*100, s[3]*10, 1/(j+1)])
-            if done:
-                r = -1
-            myAgent.log_result(s, a, r)
-            s = s1
-            if done:
-                myAgent.update()
-                break
-                
-        if j == sim_step_limit:
+                s1, r, done, _ = env.step(a)  # Get our reward for taking an action given a bandit.
+                # s1 = [position of cart, velocity of cart, angle of pole, rotation rate of pole]
+                reward = 1/(0.001+np.linalg.norm([s1[0]*50, s1[1]*10, s1[2]*100, s[3]*10]))
+                myAgent.log_result(s, a, reward)
+                s = s1
+                if done:
+                    break
+
             myAgent.update()
 
-        total_num_steps += j
-        if i % (max_ep * 0.05) == 0 and i != 0:
-            print((i / max_ep) * 100, '% Avg num steps alive: ', total_num_steps/(max_ep*0.05))
-            if total_num_steps/(max_ep*0.05) > (0.95*sim_step_limit):
-                break
-            total_num_steps = 0
-        i += 1
+            total_num_steps += j
+        print('epoch ', k, ', Avg num steps alive: ', total_num_steps/epoc_length)
+        if total_num_steps/epoc_length > (0.95*sim_step_limit):
+            break
+        total_num_steps = 0
+        k += 1
 except:
     pass
 
 s = env.reset()
+myAgent.set_to_learn(False)
 input('Enter any key to start playing result:')
 while True:
     # Probabilistically pick an action given our network outputs.
-    a = np.argmax(myAgent.make_decision(s))
+    a = myAgent.make_decision(s)
     s, r, done, _ = env.step(a)  # Get our reward for taking an action given a bandit.
     env.render()
     if done:
