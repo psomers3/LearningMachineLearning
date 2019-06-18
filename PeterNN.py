2f891bee7f9d0420eeeab9545bc3a9abbe41c013
Peter Somers <st164174@stud.uni-stuttgart.de>
2019-06-18 12:08:20 +0200

    changed DQN reward to help with convergence for limited experience buffer

--- PeterNN.py
+++ PeterNN.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 
 
 if __name__ == '__main__':
-    x = [-5, -4, -3, -2, 0, 2, 3, 4, 5]
-    y = [25, 16, 9, 4, 0, 4, 9, 16, 25]
+    x = [-4, -3, -2, 0, 2, 3, 4]
+    y = [16, 9, 4, 0, 4, 9, 16]
     hidden_layer_specs = list()
     hidden_layer_specs.append({'layer': 'dense',
                                'units': 32,
@@ -13,22 +13,29 @@ if __name__ == '__main__':
     hidden_layer_specs.append({'layer': 'dense',
                                'units': 32,
                                'activation': 'relu'})
-    net = DNN(input_shape=(None, 1), output_size=1, hidden_layers=hidden_layer_specs)
+    hidden_layer_specs.append({'layer': 'dense',
+                               'units': 1})
+    net = DNN.DNN(input_shape=(None, 1), layers=hidden_layer_specs)
     net.initiate()
+
     x = np.reshape(x, (len(x), 1))
     y = np.reshape(y, (len(y), 1))
-    episodes = 200000
+    episodes = 100000
     print('Training...')
     for i in range(episodes):
         net.train_batch(x, y)
         if i % (episodes*0.05) == 0 and i != 0:
             print((i/episodes)*100, '%')
 
+    json = net.write_model_to_json('./test.json')
+    new_dnn = DNN.create_from_json('./test.json')
+
     test_values = np.arange(-5, 5, 0.2)
     test_values = np.reshape(test_values, (len(test_values), 1))
-    prediction = net.predict(test_values)
+    prediction_1 = new_dnn.predict(test_values)
 
     plt.figure()
     plt.scatter(x, y)
-    plt.plot(test_values, prediction)
+    plt.plot(test_values, prediction_1)
+
     plt.show()
